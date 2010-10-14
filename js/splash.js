fe0b033b5e201c807ab550a4e8badc3137022f87
Scott Rouse <scottwrouse@gmail.com>
2010-10-14 00:21:11 -0600

    front page jquery swap complete

--- js/splash.js
+++ js/splash.js
@@ -4,27 +4,27 @@ $(document).ready(function(){
 		$('#primary-links a span').hide();
 		$('#feature-industrial').show();
 		$('#primary-links a#industrial span').show();
-		$('#splash').removeClass().addClass('industrial');
+		$('#splash').removeClass().addClass('industrial-splash');
 	});
 	$('a#office').click(function(){
 		$('#feature .feature').hide();
 		$('#primary-links a span').hide();
 		$('#feature-office').show();
 		$('#primary-links a#office span').show();
-		$('#splash').removeClass().addClass('office');
+		$('#splash').removeClass().addClass('office-splash');
 	});
 	$('a#technical').click(function(){
 		$('#feature .feature').hide();
 		$('#primary-links a span').hide();
 		$('#feature-technical').show();
 		$('#primary-links a#technical span').show();
-		$('#splash').removeClass().addClass('technical');
+		$('#splash').removeClass().addClass('technical-splash');
 	});
 	$('a#medical').click(function(){
 		$('#feature .feature').hide();
 		$('#primary-links a span').hide();
 		$('#feature-medical').show();
 		$('#primary-links a#medical span').show();
-		$('#splash').removeClass().addClass('medical');
+		$('#splash').removeClass().addClass('medical-splash');
 	});
 });
