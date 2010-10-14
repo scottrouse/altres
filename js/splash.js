a21434121dd4a847143ef60b9cbf2e45ef42d0c8
Scott Rouse <scottwrouse@gmail.com>
2010-10-14 00:05:24 -0600

    splash hide and show complete

--- js/splash.js
+++ js/splash.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
-
-$('a#industrial').click(function(){
-	$('#feature .feature').hide();
-	$('#feature-industrial').show();
-});
-$('a#office').click(function(){
-	$('#feature .feature').hide();
-	$('#feature-office').show();
-});
-$('a#technical').click(function(){
-	$('#feature .feature').hide();
-	$('#feature-technical').show();
-});
-$('a#medical').click(function(){
-	$('#feature .feature').hide();
-	$('#feature-medical').show();
-});
-
+	$('a#industrial').click(function(){
+		$('#feature .feature').hide();
+		$('#primary-links a span').hide();
+		$('#feature-industrial').show();
+		$('#primary-links a#industrial span').show();
+	});
+	$('a#office').click(function(){
+		$('#feature .feature').hide();
+		$('#primary-links a span').hide();
+		$('#feature-office').show();
+		$('#primary-links a#office span').show();
+	});
+	$('a#technical').click(function(){
+		$('#feature .feature').hide();
+		$('#primary-links a span').hide();
+		$('#feature-technical').show();
+		$('#primary-links a#technical span').show();
+	});
+	$('a#medical').click(function(){
+		$('#feature .feature').hide();
+		$('#primary-links a span').hide();
+		$('#feature-medical').show();
+		$('#primary-links a#medical span').show();
+	});
 });
