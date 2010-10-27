adab70f1b127908512918601c5ebc77c5c139601
Scott Rouse <scottwrouse@gmail.com>
2010-10-27 10:08:05 -0600

    zebra striping for table.zebra and list styles for table.list using css/js

--- js/zebra.js
+++ js/zebra.js
@@ -9,4 +9,6 @@
 $(document).ready(function() {
         $ ('ul.zebra li:even').addClass ('even');
         $ ('ul.zebra li:odd').addClass ('odd');
+        $ ('table.zebra tr:even td').addClass ('even');
+        $ ('table.zebra tr:odd td').addClass ('odd');
     });
