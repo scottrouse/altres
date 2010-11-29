da1dec3818c9f60e81e690f161091fbd94a8e556
Scott Rouse <scottwrouse@gmail.com>
2010-11-29 15:43:17 -0700

    edited js to add class of .active to a on staffing bar on front page to fix 'Rollover states disappear (not staying on) when you’re on one of the pages (Industrial, office, technical, and medical) in IE and Firefox. Safari is ok' and 'Rollover line spacing problem is still occurring in IE 7 and Firefox 3.6. As soon as you rollover another item, the line spacing problem occurs.'

--- js/splash.js
+++ js/splash.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 		$('#primary-links a span').hide();
 		$('#feature-industrial').show();
 		$('#primary-links a#industrial span').show();
+		$('#primary-links ul li a.active').removeClass('active');
+		$('#primary-links ul li a#industrial').addClass('active');
 		$('#splash').removeClass().addClass('industrial-splash');
 	});
 	$('a#office').click(function(){
@@ -11,6 +13,8 @@ $(document).ready(function(){
 		$('#primary-links a span').hide();
 		$('#feature-office').show();
 		$('#primary-links a#office span').show();
+		$('#primary-links ul li a.active').removeClass('active');
+		$('#primary-links ul li a#office').addClass('active');
 		$('#splash').removeClass().addClass('office-splash');
 	});
 	$('a#technical').click(function(){
@@ -18,6 +22,8 @@ $(document).ready(function(){
 		$('#primary-links a span').hide();
 		$('#feature-technical').show();
 		$('#primary-links a#technical span').show();
+		$('#primary-links ul li a.active').removeClass('active');
+		$('#primary-links ul li a#technical').addClass('active');
 		$('#splash').removeClass().addClass('technical-splash');
 	});
 	$('a#medical').click(function(){
@@ -25,6 +31,8 @@ $(document).ready(function(){
 		$('#primary-links a span').hide();
 		$('#feature-medical').show();
 		$('#primary-links a#medical span').show();
+		$('#primary-links ul li a.active').removeClass('active');
+		$('#primary-links ul li a#medical').addClass('active');
 		$('#splash').removeClass().addClass('medical-splash');
 	});
 });
